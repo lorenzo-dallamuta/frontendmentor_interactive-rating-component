deb9ded4003ff71d560bbed8a3fd454cecc8961d
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2022-04-28 15:40:35 +0200

    visual feedback for answer state

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const grade_result = document.querySelector('.result-box__grade-number')
 const modal = document.querySelector(".result-box")
 const rating_buttons = document.querySelectorAll('.rate-box__rating')
 const submit_button = document.querySelector('.rate-box__submit')
+const signal_icon = document.querySelector('.rate-box__icon')
 
 rating_buttons.forEach((n, i) => n.addEventListener('click', e => {
   rating = i + 1
   submit_button.disabled = false
+  signal_icon.setAttribute('data', "./images/icon-star.svg")
 }))
 
 submit_button.addEventListener('click', (e) => {

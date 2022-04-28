db9d65a41df3f12fc5d703190f2999b3e9012cf1
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2022-04-28 16:47:17 +0200

    firefox patch

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const rating_buttons = document.querySelectorAll('.rate-box__rating')
 const submit_button = document.querySelector('.rate-box__submit')
 const signal_icon = document.querySelector('.rate-box__icon')
 
+window.onload = (e) => submit_button.disabled = true
+
 rating_buttons.forEach((n, i) => n.addEventListener('click', e => {
   rating = i + 1
   submit_button.disabled = false
@@ -15,6 +17,6 @@ rating_buttons.forEach((n, i) => n.addEventListener('click', e => {
 submit_button.addEventListener('click', (e) => {
   e.preventDefault()
   grade_result.innerText = rating.toString()
-  modal.showModal()
+  modal.showModal() //not yet on firefox
   // do post stuff
 })
